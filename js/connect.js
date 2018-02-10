5a51c1166a59ea5d11d489b754416f8c365bc451
Brock Halladay <bhalladay@instructure.com>
2018-02-09 20:09:39 -0700

    Added structure for GET calls to make them easier for admins

--- js/connect.js
+++ js/connect.js
@@ -1,9 +1,18 @@
+function getCompleted(status, cb, failcb, _attempts) {
+  var prom = status;
+  _attempts = ++_attempts || 1;
+  console.log(prom, _attempts);
+  if (prom.readyState == 4) return cb(prom.responseJSON);
+  if (_attempts == 60) return failcb();
+  setTimeout(function() {
+    getCompleted(status, cb, failcb, _attempts);
+  }, 250);
+};
+let accountSettings = $.get('/api/admin/account_configs/current');
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log(request, sender, sendResponse);
-    /*console.log(sender.tab ?
-      "from a content script:" + sender.tab.url :
-      "from the extension");*/
     switch (request.req) {
       case 'hideBAT':
         hideBat();
@@ -15,10 +24,25 @@ chrome.runtime.onMessage.addListener(
   return true; // This is needed to allow for asynchronous responses via sendResponse
 });
 
-var hideBat = function() {
+let hideBat = function() {
   $('.BAT').hide();
 };
 
-var showBat = function() {
+let showBat = function() {
   $('.BAT').show();
 };
+
+let displayCall = (pathName)=>{
+  return new Promise((res,rej)=>{
+    let promiseState = $.get(pathName);
+    getCompleted(promiseState,(data)=>{
+      res(data);
+    },()=>{rej('Promise Rejected: something went wrong with the promise call')});
+  });
+}
+/*
+This call will be added once the interface is built to include a string to search by - Quick API
+//   displayCall('/api/admin/account_configs/current').then((data)=>{
+//     console.log(data);
+//   }).catch((err)=>{console.log(err)});
+*/
