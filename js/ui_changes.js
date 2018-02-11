6db563b98b7ed1fae5cc079ff6a68d69ad625c4f
JamesHaengsin <rowley.james74@gmail.com>
2018-02-10 18:26:09 -0700

    Added box to enhancement page and modified css

--- js/ui_changes.js
+++ js/ui_changes.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
   $('#buc-header-nav').parent().prepend('<div id="enhancement_banner" class="BAT">Enhancements Activated!</div>');
+    $('#enhancement_banner').append('<div id="API_display"><pre>' + '</pre></div>');
 });
