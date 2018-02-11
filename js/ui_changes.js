7e2eb24cdeef35e87c0a5946d82659474f0b9f97
Brock Halladay <bhalladay@instructure.com>
2018-02-10 18:59:31 -0700

    added hide JSON button

--- js/ui_changes.js
+++ js/ui_changes.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-  $('#buc-header-nav').parent().prepend('<div id="enhancement_banner" class="BAT">Enhancements Activated!</div>');
+  $('#buc-header-nav').parent().prepend('<div id="enhancement_banner" class="BAT"><span id="title"> Enhancements Activated!</span><div><button id="hide_enhance_div">Hide JSON</button></div></div>');
   $('#enhancement_banner').append('<div id="API_display"><pre>' + '</pre></div>');
 });
